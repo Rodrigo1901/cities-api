2e5f6e12dfd3629adaddec3fd869301f31c3afc7
Rodrigo1901 <39284513+Rodrigo1901@users.noreply.github.com>
2021-05-21 21:53:28 -0300

    Initial commit

---
+++ src/test/java/com/github/rodrigo1901/citiesapi/CitiesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.rodrigo1901.citiesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CitiesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
