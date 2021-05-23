6406efc410043db792093b0bb700dc1455a9c7a7
Rodrigo1901 <39284513+Rodrigo1901@users.noreply.github.com>
2021-05-22 21:00:27 -0300

    Third commit and push

--- src/main/java/com/github/rodrigo1901/citiesapi/cities/CityRepository.java
+++ src/main/java/com/github/rodrigo1901/citiesapi/cities/CityRepository.java
@@ -1,6 +1,7 @@
 package com.github.rodrigo1901.citiesapi.cities;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface CityRepository extends JpaRepository<City, Long> {
     @Query(value = "SELECT ((SELECT lat_lon FROM cidade WHERE id=?1) <@> (SELECT lat_lon FROM cidade WHERE id=?2)) as distance", nativeQuery = true)
